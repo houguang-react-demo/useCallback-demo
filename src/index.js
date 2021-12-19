698c20a12e45be8eb704a3e975c1413b1bfdb3e0
houguang <houguang@sina.cn>
2021-12-19 23:04:55 +0800

    useMemo用法演示

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const App = () => {
+  
+  const [count, setCount] = useState(1);
+  const [val, setValue] = useState('');
+  
+  const expensive = useMemo(() => {
+    console.log('compute');
+    let sum = 0;
+    for (let i = 0; i < count * 100; i++) {
+      sum += i;
+    }
+    return sum;
+  },[count])
+  
+  return <div>
+    <h4>{count}-{val}-{expensive}</h4>
+    <div>
+      <button onClick={() => setCount(count + 1)}>按钮</button>
+      {/*未使用Memo时，修改val时expensive也会重新渲染,使用useMemo时，修改val时，不会重新渲染expensive*/}
+      <input value={val} onChange={event => setValue(event.target.value)}/>
+    </div>
+  </div>;
+  
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <><App/></>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
